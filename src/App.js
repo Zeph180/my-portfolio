531f614ad0d0f2802be4850ff5aff65265d35bb0
Zeph180 <kigozijosephsuubi1@gmail.com>
2022-10-31 15:02:07 +0300

    trying to fix home page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
@@ -7,21 +8,41 @@ import ProjectsPage from "./pages/ProjectsPage";
 import ContactPage from "./pages/ContactPage";
 import "./App.css";
 // import darkBg from "./assets";
+import { AiFillGithub, AiOutlineInstagram } from "react-icons/ai";
+import { FiLinkedin, FiTwitter } from "react-icons/fi";
+
+const Socials = ({icon}) => {
+	return (
+		<li style={{margin: "10px"}}>
+			{icon}
+		</li>
+	);
+};
 
 function App() {
 	return (
 		<BrowserRouter>
 			<div id="app-container">
 				<Navbar />
-				<Routes>
-					<Route exact path="/" element=<HomePage /> />
-					<Route path="/about" element=<AboutPage /> />
-					<Route path="/projects" element=<ProjectsPage /> />
-					<Route path="/contact" element=<ContactPage /> />
-					{/* <Route path="/resume" element=<AboutPage /> /> */}
-				</Routes>
+				<div style={{display: "flex"}}>
+					<ul style={{paddingLeft: "7.5%", paddingTop: "16%"}}>
+						<Socials icon={<AiFillGithub size={15} color="#0F85F2"/>} />
+						<Socials icon={<FiTwitter size={15} color="#0F85F2"/>} />
+						<Socials icon={<FiLinkedin size={15} color="#0F85F2"/>} />
+						<Socials icon={<AiOutlineInstagram size={15} color="#0F85F2"/>} />
+					</ul>
+					<Routes>
+						<Route path="/my-portfolio" element=<HomePage /> />
+						<Route path="/about" element=<AboutPage /> />
+						<Route path="/projects" element=<ProjectsPage /> />
+						<Route path="/contact" element=<ContactPage /> />
+						{/* <Route path="/resume" element=<AboutPage /> /> */}
+					</Routes>
+				</div>
+				<div>
+					<p>fehbkrgrfguifr</p>
+				</div>
 			</div>
-
 		</BrowserRouter>
 	);
 }
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,7 +3,7 @@ import React from "react";
 const AboutPage = () => {
 	return (
 		<div>
-			<p>About</p>
+			<p style={{color: "pink"}}>About</p>
 		</div>
 	);
 };
